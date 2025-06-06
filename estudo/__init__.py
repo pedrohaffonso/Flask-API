54eee0a1e9c9def532c191fee4161a489ec8adc0
pedrohaffonso <affonso.pedromatos@gmail.com>
2025-06-06 11:38:16 -0300

    Importando Banco de Dados e BootStrap

--- estudo/__init__.py
+++ estudo/__init__.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'EEHJKWNWMKC8382DFJNFNJJ'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 from estudo.views import homepage
+from estudo.models import Contato
--- estudo/views.py
+++ estudo/views.py
@@ -1,6 +1,44 @@
-from estudo import app
-from flask import render_template
+from estudo import app, db
+from flask import render_template, url_for, request, redirect
+
+from estudo.models import Contato
+from estudo.forms import ContatoForm
 
 @app.route("/")
 def homepage():
-    return render_template("index.html")
+    usuario = 'Jonathas'
+    idade = 34
+
+    context = {
+        'usuario': usuario,
+        'idade': idade
+    }
+    return render_template("index.html", context=context)
+
+@app.route("/contato/", methods=["GET", "POST"])
+def contato():
+    context = {}
+    form = ContatoForm()
+    
+    if form.validate_on_submit():
+        form.save()
+        return redirect(url_for('homepage'))
+        
+    return render_template('contato.html', context=context, form=form)
+
+@app.route("/contato/lista")
+def contatoLista():
+
+    if request.method == 'GET':
+        pesquisa = request.args.get('pesquisa', '')
+
+    dados = Contato.query.order_by(Contato.nome)
+    if pesquisa != '':
+        dados = dados.filter_by(nome =pesquisa)
+    context = {'dados': dados.all()}
+    
+    return render_template('contato_lista.html', context=context)
+
+
+
+
